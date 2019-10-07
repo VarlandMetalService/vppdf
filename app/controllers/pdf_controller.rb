c8a042be32a07976b5efa5912f547441a470bcd4
Toby Varland <toby.varland@varland.com>
2019-10-07 11:40:49 -0400

    Set up auto printing

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,7 +1,9 @@
 class PdfController < ApplicationController
 
   def shipper
-    self.send_pdf(Shipper.new(params[:shipper]), 'Shipper')
+    shipper = Shipper.new(params[:shipper])
+    self.print_file(shipper, nil, "192.168.82.79", "PackingSlip", "PS ##{params[:shipper]}")
+    self.send_pdf(shipper, 'Shipper')
   end
 
   def purchase_order
@@ -18,11 +20,25 @@ class PdfController < ApplicationController
 
 protected
 
+  # Sends PDF to browser window.
   def send_pdf(pdf, name)
     send_data pdf.render,
               filename: "#{name}.pdf",
               type: 'application/pdf',
               disposition: 'inline'
+  end
+
+  # Sends PDF file to print queueing software.
+  def print_file(file, user, ip, type, description)
+    job = {
+      file: "data:application/pdf;base64,#{Base64.encode64(file.render)}",
+      user: user,
+      ip_address: ip,
+      document_type: type,
+      description: description
+    }
+    uri = URI.parse("http://vms.varland.com/print_jobs")
+    Net::HTTP.post_form(uri, {"data" => JSON.generate(job)})
   end
 
 end
